19c649cb61629ddfe1dc9d42287f9e7888741bb0
aman <amansainigithub>
2022-04-23 23:31:04 +0530

    minor changes

--- src/app/helper-msg/auth-url.service.ts
+++ src/app/helper-msg/auth-url.service.ts
@@ -7,8 +7,8 @@ export class AuthURLService {
 
   constructor() { }
 
-  public authUrl:string = 'https://64.227.8.158:3355/api/auth/public/cred/';
-  //  public authUrl:string = 'http://localhost:3355/api/auth/public/cred/';
+  // public authUrl:string = 'https://64.227.8.158:3355/api/auth/public/cred/';
+   public authUrl:string = 'http://localhost:3355/api/auth/public/cred/';
   
 
 }
